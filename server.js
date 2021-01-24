44b899e9b8d24ebbbd6f3d978482691ca859d070
charliethomasdavis <charles.tb.davis@gmail.com>
2021-01-24 15:59:53 -0600

    Add basic api route

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express();
+const router = express.Router();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+// Enable express to parse json requests
+app.use(express.json())
+
+// Define routes
+router.get('/test', (req, res) => {
+  res.json({'message': 'hooray! an api' });
 });
 
+router.post('/send', (req, res) => {
+  console.log(req.body);
+  res.json({'message': 'received'});
+});
+
+// Register routes to root URL
+app.use('/api', router);
+
+// Start server on assigned port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
+  console.log(`Example app listening on port ${port}!`);
 });
